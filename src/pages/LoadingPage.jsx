39d1404c24eccaa12b4c60602761ef57057ea271
Natinale <natinaleyosef@gmail.com>
2026-02-14 23:23:41 +0200

    Fix mobile responsiveness

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import CoreOpticSystem from '../components/CoreOpticSystem';
 const LoadingPage = ({ onComplete }) => {
   useEffect(() => {
-    const timer = setTimeout(() => { if (onComplete) onComplete(); }, 3000);
+    const timer = setTimeout(() => { if (onComplete) onComplete(); }, 8000);
     return () => clearTimeout(timer);
   }, [onComplete]);
   return <CoreOpticSystem onComplete={onComplete} />;
